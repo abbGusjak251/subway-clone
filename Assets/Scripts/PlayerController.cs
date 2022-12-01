3f18620291c5042cf7681627c36e5342f020eeed
abb21timsjo <tim.sjostrand@abbgymnasiet.se>
2022-12-01 15:39:34 +0100

    animationer till spring och hopp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    private float jumpForce = 5f;
+    private float jumpForce = 4f;
     private Vector3 positionOrigin;
     private Vector3[] positions = new Vector3[3];
     private int positionIndex = 0;
     private float railDistance = 2f;
     private bool grounded;
+    private Animator animator;
+    private float x;
+    public float SpeedSlide;
     public enum State {
         Idle,
         Running,
@@ -20,6 +23,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         positionOrigin = transform.position;
         // Add possible horizontal positions
         positions[0] = positionOrigin - Vector3.left*railDistance;
@@ -48,21 +52,26 @@ public class PlayerController : MonoBehaviour
             if(Input.GetKeyDown("left")) {
                 if(positionIndex < positions.Length-1) {
                     positionIndex++;
+                    animator.SetTrigger("SlideLeft");
                 }
             }
             if(Input.GetKeyDown("right")) {
                 if(positionIndex > 0) {
                     positionIndex--;
+                    animator.SetTrigger("SlideRight");
                 }
             }
             Vector3 toPos = new Vector3(positions[positionIndex].x, transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, toPos, .2f);
         }
+        x = Mathf.Lerp(x,positionIndex,Time.deltaTime*10f);
     }
 
     void Jump() {
         Debug.Log(grounded);
+        animator.SetTrigger("JumpAnimation");
         rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+        
     }
 
     void OnCollisionEnter(Collision other) {
