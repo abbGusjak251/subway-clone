48512ec9057a93023e041d9b3ebdb0200d6678da
abbangtor <angla.tornros@abbgymnasiet.se>
2022-12-14 13:53:43 +0100

    Fixat spawning av objekt

--- Assets/Ängla/Scripts/LevelManager.cs
+++ Assets/Ängla/Scripts/LevelManager.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-   public static LevelManager Instance { set; get;}
+    public static LevelManager Instance { set; get; }
 
-//    private const bool Show_Collider = true;
+    public bool SHOW_COLLIDER = true;   //$$
 
-//    // Level spawning
+    // Level spawning
 
-//    private const float DISTANCE_BEFORE_SPAWN = 100.0f;
-//    private const int INITIAL_SEGMENTS = 10;
-//    private const int MAX_SEGMENTS_ON_SCREEN = 15;
-//    private Transform cameraContainer;
-//    private int amountOfActiveSegments;
-//    private int continiousSegments;
-//    private int currentSpawnZ;
-//    private int currentLevel;
-//    private int y1, y2, y3;
+    private const float DISTANCE_BEFORE_SPAWN = 100.0f;
+    private const int INITIAL_SEGMENTS = 10;
+    private const int MAX_SEGMENTS_ON_SCREEN = 15;
+    private Transform cameraContainer;
+    private int amountOfActiveSegments;
+    private int continiousSegments;
+    private int currentSpawnZ;
+    private int currentLevel;
+    private int y1, y2, y3;
 
 
 
- //List of pieces 
+    //List of pieces 
     public List<Piece> ramps = new List<Piece>();
     public List<Piece> trains = new List<Piece>();
     public List<Piece> jumps = new List<Piece>();
     public List<Piece> slides = new List<Piece>();
-
+    [HideInInspector]
     public List<Piece> pieces = new List<Piece>(); // All the pieces
 
     public Piece GetPiece(PieceType pt, int visualIndex)
     {
         Piece p = pieces.Find(x => x.type == pt && x.visualIndex == visualIndex && !x.gameObject.activeSelf);
-       
-        if(p == null)
+
+        if (p == null)
         {
             GameObject go = null;
-            if(pt==PieceType.ramp)
+            if (pt == PieceType.ramp)
                 go = ramps[visualIndex].gameObject;
             else if (pt == PieceType.train)
                 go = trains[visualIndex].gameObject;
@@ -51,111 +51,122 @@ public class LevelManager : MonoBehaviour
             p = go.GetComponent<Piece>();
             pieces.Add(p);
         }
-        
+
         return p;
     }
 
-    //    //List of segments
-    //    //public List<Segment> availableSegments = new List<Segment>();
-    //    //public List<Segment> availableTransitions = new List<Segment>();
-    //    //public List<Segment> segments = new List<Segment>();
+    //List of segments
+    public List<Segment> availableSegments = new List<Segment>();
+    public List<Segment> availableTransitions = new List<Segment>();
+    [HideInInspector]
+    public List<Segment> segments = new List<Segment>();
 
-    //    // Gameplay
-    //    //private bool isMoving=false;
+    // Gameplay
+    private bool isMoving = false;
 
-    //    private void Awake()
-    //    {
-    //        Instance = this;
-    //        CameraContainer = Camera.current.transform;    //current camera
-    //        Current_SpawnZ = 0;
-    //        Current_Level = 0;
-    //    }
+    private void Awake()
+    {
+        Instance = this;
+        cameraContainer = Camera.main.transform;    //current camera
+        currentSpawnZ = 0;
+        currentLevel = 0;
+    }
 
-    //    private void Start()
-    //    {
-    //        for (int i = 0; i < Initial_Segments; i++)
-    //            GenerateSegment();
-    //    }
+    private void Start()
+    {
+        for (int i = 0; i < INITIAL_SEGMENTS; i++)
+            GenerateSegment();
+    }
 
-    //    private void GenerateSegment()
-    //    {
-    //        SpawnSegment();
+    private void Update() 
+    {
+        if(currentSpawnZ - cameraContainer.position.z<DISTANCE_BEFORE_SPAWN)
+            GenerateSegment();
+        
+        if(amountOfActiveSegments >= MAX_SEGMENTS_ON_SCREEN)
+        {
+            segments[amountOfActiveSegments - 1].DeSpawn();
+            amountOfActiveSegments--;
+        }
+    }
+    private void GenerateSegment()
+    {
+        SpawnSegment();
 
-    //        if (Random.Range(0f, 1f) < (Continious_Segments * 0.25f))
+        if (Random.Range(0f, 1f) < (continiousSegments * 0.25f))
 
-    //        {
-    //            Continious_Segments++;
-    //            //Spawn transition Segment
-    //            Continious_Segments = 0;
-    //            SpawnTransition();
-    //        }
-    //        else
-    //        {
-    //            Continious_Segments++;
-    //        }
-    //    }
+        {
+            continiousSegments++;
+            //Spawn transition Segment
+            continiousSegments = 0;
+            SpawnTransition();
+        }
+        else
+        {
+            continiousSegments++;
+        }
+    }
 
-    //    private void SpawnSegment()
-    //    {
-    //        List<Segment> possibleSegment = availableSegments.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
-    //        int id = Random.Range(0, possibleSegment.Count);
+    private void SpawnSegment()
+    {
+        List<Segment> possibleSeg = availableSegments.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleSeg.Count);
 
-    //        Segment s  = GetSegment(id,false);
+        Segment s = GetSegment(id, false);
 
-    //        y1 = s.endY1;
-    //        y2 = s.endY2;
-    //        y3 = s.endY3;
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
 
-    //        s.transform.SetParent(transform);
-    //        s.transform.localPosition = Vector3.forward * Current_SpawnZ;
+        s.transform.SetParent(transform);
+        s.transform.localPosition = Vector3.forward * currentSpawnZ;
 
-    //        Current_SpawnZ += s.lenght;
-    //        Amount_Of_Active_Segments++;
-    //        s.Spawn();
-    //    }
-    //    private void SpawnTransition()
-    //    {
-    //        List<Segment> possibleTransition = availableTransitions.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
-    //        int id = Random.Range(0, possibleTransition.Count);
+        currentSpawnZ += s.lenght;
+        amountOfActiveSegments++;
+        s.Spawn();
+    }
+    private void SpawnTransition()
+    {
+        List<Segment> possibleTransition = availableTransitions.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleTransition.Count);
 
-    //        Segment s = GetSegment(id, true);
+        Segment s = GetSegment(id, true);
 
-    //        y1 = s.endY1;
-    //        y2 = s.endY2;
-    //        y3 = s.endY3;
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
 
-    //        s.transform.SetParent(transform);
-    //        s.transform.localPosition = Vector3.forward * Current_SpawnZ;
+        s.transform.SetParent(transform);
+        s.transform.localPosition = Vector3.forward * currentSpawnZ;
 
-    //        Current_SpawnZ += s.lenght;
-    //        Amount_Of_Active_Segments++;
-    //        s.Spawn();
-    //    }
+        currentSpawnZ += s.lenght;
+        amountOfActiveSegments++;
+        s.Spawn();
+    }
 
-    //    public Segment GetSegment(int id, bool transition)
-    //    {
-    //        Segment s = null;
-    //        s = segments.Find(x => x.SegmentId == id && x.transition == transition && !x.gameObject.activeSelf);
-    //        // SegmentId might be wrong !
-    //        if (s == null)
-    //        {
-    //            GameObject go = Instantiate((transition) ? availableTransitions[id].gameObject : availableSegments[id].gameObject) as GameObject;
-    //            // ternary operator
-    //            s = go.GetComponent<Segment>();
+    public Segment GetSegment(int id, bool transition)
+    {
+        Segment s = null;
+        s = segments.Find(x => x.SegId == id && x.transition == transition && !x.gameObject.activeSelf);
 
-    //            s.SegmentId = id;
-    //            s.transition = transition;
+        if (s == null)
+        {
+            // ternary operator
+            GameObject go = Instantiate((transition) ? availableTransitions[id].gameObject : availableSegments[id].gameObject) as GameObject;
 
-    //            segments.Insert(0, s);
-    //        }
-    //        else
-    //        {
-    //            segments.Remove(s);
-    //            segments.Insert(0, s);
-    //        }
+            s = go.GetComponent<Segment>();
 
-    //        return s;
+            s.SegId = id;
+            s.transition = transition;
+
+            segments.Insert(0, s);
+        }
+        else
+        {
+            segments.Remove(s);
+            segments.Insert(0, s);
+        }
+
+        return s;
+    }
 }
-//    //  public Piece GetPiece(PieceType pt, int visualIndex);
-
-//}
--- Assets/Ängla/Scripts/PieceSpawner.cs
+++ Assets/Ängla/Scripts/PieceSpawner.cs
@@ -9,12 +9,28 @@ public class PieceSpawner : MonoBehaviour
 
     public void Spawn()
     {
-        currentPiece = LevelManager.Instance.GetPiece(type, 0);
+        int pieceCount = 0;
+        switch (type)
+        {
+            case PieceType.jump:
+                pieceCount = LevelManager.Instance.jumps.Count;
+                break;
+            case PieceType.slide:
+                pieceCount = LevelManager.Instance.slides.Count;
+                break;
+            case PieceType.train:
+                pieceCount = LevelManager.Instance.trains.Count;
+                break;
+            case PieceType.ramp:
+                pieceCount = LevelManager.Instance.ramps.Count;
+                break;
+        }
+        currentPiece = LevelManager.Instance.GetPiece(type, Random.Range(0, pieceCount));
         currentPiece.gameObject.SetActive(true);
         currentPiece.transform.SetParent(transform, false);
     }
 
-    public void Despawn()
+    public void DeSpawn()
     {
         currentPiece.gameObject.SetActive(false);
     }
